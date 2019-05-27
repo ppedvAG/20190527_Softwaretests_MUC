17d3a2295e6d7017a97acf7f3ec23b5eb5418ad1
Michael Zöhling <michael.zoehling@outlook.com>
2019-05-27 11:05:45 +0200

    DataRow und Kategorie

--- Taschenrechner/TaschenrechnerTests/CalculatorTests.cs
+++ Taschenrechner/TaschenrechnerTests/CalculatorTests.cs
@@ -7,6 +7,7 @@ namespace Taschenrechner.Tests
     public class CalculatorTests
     {
         [TestMethod]
+        [TestCategory("MSTest")]
         public void Sum_3_and_5_returns_8()
         {
             // Arrange
@@ -21,6 +22,7 @@ namespace Taschenrechner.Tests
         // Extremfall: Maximum / Minimum
 
         [TestMethod]
+        [TestCategory("MSTest")]
         public void Sum_0_and_0_returns_0()
         {
             // Arrange
@@ -32,6 +34,7 @@ namespace Taschenrechner.Tests
         }
 
         [TestMethod]
+        [TestCategory("MSTest")]
         public void Sum_MAX_and_1_throws_OverflowException()
         {
             // Arrange
@@ -41,12 +44,25 @@ namespace Taschenrechner.Tests
         }
 
         [TestMethod]
+        [TestCategory("MSTest")]
         public void Sum_MIN_and_N1_throws_OverflowException()
         {
             // Arrange
             var calc = new Calculator();
             // Act + Assert
             Assert.ThrowsException<OverflowException>(() => calc.Sum(Int32.MinValue, -1));
+        }
+
+        [TestMethod]
+        [TestCategory("MSTest")]
+        [DataRow(3,5,8)]
+        [DataRow(-1,-2,-3)]
+        [DataRow(1000,2000,3000)]
+        public void Can_Sum(int a, int b, int expected)
+        {
+            Calculator calc = new Calculator();
+            var result = calc.Sum(a, b);
+            Assert.AreEqual(expected, result);
         }
 
     }
