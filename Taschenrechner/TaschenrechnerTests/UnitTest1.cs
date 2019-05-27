4a06161c3c5e83e7fe0b8ff32f08a07786a9890a
Michael Zöhling <michael.zoehling@outlook.com>
2019-05-27 10:04:32 +0200

    Init TaschenrechnerTests

---
+++ Taschenrechner/TaschenrechnerTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaschenrechnerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
