bfa5971fb273d576aa0249c8bfab632a5fce6f22
Michael Zöhling <michael.zoehling@outlook.com>
2019-05-27 10:15:39 +0200

    Unser erster UnitTest :)

---
+++ Taschenrechner/TaschenrechnerTests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taschenrechner.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var calc = new Calculator();
+
+            var erg = calc.Sum(3, 5);
+
+            Assert.AreEqual(8, erg);
+        }
+    }
+}
