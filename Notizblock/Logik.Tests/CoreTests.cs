0f2bbe96527850feaf835ba707a7172b50e214eb
Michael Zöhling <michael.zoehling@outlook.com>
2019-05-28 11:53:16 +0200

    Komplexen Unit-Test

--- Notizblock/Logik.Tests/CoreTests.cs
+++ Notizblock/Logik.Tests/CoreTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using AutoFixture;
+using Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logik.Tests
@@ -7,8 +10,20 @@ namespace Logik.Tests
     public class CoreTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void WorkOnTodoItems_returns_all_uncompleted_items_as_completed()
         {
+            var fixture = new Fixture();
+            var workload = fixture.CreateMany<TodoItem>(1000).ToList();
+            var core = new Core(null); // Service wird hier nicht verwendet !
+            int formerUncompletedItems = workload.Count(x => x.Completed == false);
+
+            Assert.IsTrue(workload.Any(x => x.Completed == false));
+
+            var result = core.WorkOnTodoItems(workload);
+
+            Assert.AreEqual(formerUncompletedItems, result.Count); // Alle ehemalig completed Items müssen bearbeitet werden
+            Assert.IsTrue(workload.All(x => x.Completed));           // Alle 1000 elemente sind nun completed
+            Assert.IsTrue(result.All(x => x.Completed));           // Jedes einzelne ehem. uncompleted muss completed sein !
         }
     }
 }
