d6fe3c34ca4827d0035358f0543bbb9e91983c07
Michael Zöhling <michael.zoehling@outlook.com>
2019-05-28 09:37:23 +0200

    Domain erstellt

---
+++ Notizblock/Domain/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TodoItem
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+    }
+}
